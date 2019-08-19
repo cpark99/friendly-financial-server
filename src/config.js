f689ff0648b359da0a597cff05dbea1a26820f1a
Chris Park <cpwebdeveloper99@gmail.com>
2019-08-19 12:11:26 -0700

    updated config

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://friendly-financial@localhost/friendly-financial',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
-  JWT_EXPIRY: process.env.JWT_EXPIRY || '10m',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '24h',
 }
