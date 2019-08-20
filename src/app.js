bead5412eba0db72c8ef359952077a0491faaf35
Chris Park <cpwebdeveloper99@gmail.com>
2019-08-20 10:27:55 -0700

    fixing cors

--- src/app.js
+++ src/app.js
@@ -6,15 +6,15 @@ const helmet = require("helmet");
 const { NODE_ENV } = require('./config')
 const authRouter = require('./auth/auth-router')
 const usersRouter = require('./users/users-router')
-// const profilesRouter = require('./profiles/profiles-router')
 
 const app = express();
 
 const morganOption = (NODE_ENV === 'production') ? "tiny" : "common";
 
 app.use(morgan(morganOption));
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
+
 
 app.use('/api/users', usersRouter)
 app.use('/api/auth',authRouter)
